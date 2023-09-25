c320582c121236f676634882e94616df6e81238a
ahmed-abdi1991 <squarehajji@gmail.com>
2023-09-25 06:47:17 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(-2);
+}
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(-2);
+}
+
+function selectingDrivers() {
+  const driversArray = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  return driversArray;
+}
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(-2);
+}
+
+function selectingDrivers() {
+  const driversArray = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  return driversArray;
+}
+function createFareMultiplier(multiplier) {
+  return function (value) {
+    return value * multiplier;
+  };
+}
+function createFareMultiplier(multiplier) {
+  return function (value) {
+    return value * multiplier;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+function createFareMultiplier(multiplier) {
+  return function (value) {
+    return value * multiplier;
+  };
+}
+
+const fareTripler = createFareMultiplier(3);
+
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(-2);
+}
+
+function selectDifferentDrivers(arrayOfDrivers, driverSelector) {
+  return driverSelector(arrayOfDrivers);
+}
